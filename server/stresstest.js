37045b5e09ef0420149d3ae5b1e56f82071ce50d
Joe Wagstaff <joe.wagstaff@gmail.com>
2022-03-10 10:24:37 -0800

    stresstest

--- server/stresstest.js
+++ server/stresstest.js
@@ -2,10 +2,11 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 1000,
+  vus: 2000,
   duration: '30s',
 };
 export default function () {
-  http.get('http://localhost:3000/api/reviews/meta/1');
+  const id = Math.floor(Math.random() * 5774593)
+  http.get(`http://localhost:3000/api/reviews/meta/${id}`);
   sleep(1);
 }
